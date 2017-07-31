7998c349ef7837b283db0bdee119c2ada3113f9f
indpurvesh <ind.purvesh@gmail.com>
2017-08-01 09:14:57 +1200

    added classmap for database migration files

--- src/database/2017_06_22_010000_mage2_feature_schema.php
+++ src/database/2017_06_22_010000_mage2_feature_schema.php
@@ -47,8 +47,7 @@ class Mage2FeatureSchema extends Migration
             'identifier' => 'is_featured',
             'field_type' => 'SELECT',
             'sort_order' => 10,
-            'is_system' => 1,
-            'use_as_variation' => '0',
+            'use_as' => 'SPECIFICATION',
         ]);
 
         AttributeDropdownOption::create([
