d9908841005af312ccd83dd445c4aa69b114e104
indpurvesh <ind.purvesh@gmail.com>
2017-08-13 11:06:46 +1200

    move module.yaml file to root

--- src/Module.php
+++ src/Module.php
@@ -64,7 +64,7 @@ class Module extends BaseModule
      */
     public function register()
     {
-        $this->registerModuleYamlFile(__DIR__ . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'module.yaml');
+        $this->registerModuleYamlFile(__DIR__ . DIRECTORY_SEPARATOR . 'module.yaml');
 
         if (true === $this->getEnable()) {
             $this->mapWebRoutes();
