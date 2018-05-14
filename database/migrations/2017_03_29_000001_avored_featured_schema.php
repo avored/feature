0a74bb07f1d5d0d3eefe334d308d9d9cb8a9f8ff
Purvesh <ind.purvesh@gmail.com>
2018-05-15 09:33:15 +1200

    updated composer json

--- database/migrations/2017_03_29_000001_avored_featured_schema.php
+++ database/migrations/2017_03_29_000001_avored_featured_schema.php
@@ -2,9 +2,6 @@
 
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
-
 use AvoRed\Framework\Models\Database\Property;
 use AvoRed\Framework\Models\Database\PropertyDropdownOption;
 
